0951620f189683c54dc2370a2af9c41df51142b2
unknown <agelos.kostas21@gmail.com>
2023-06-20 15:59:58 +0300

    fixed guage merge bug

--- src/components/GaugeChart.js
+++ src/components/GaugeChart.js
@@ -10,93 +10,81 @@ HCSoldGauge(Highcharts);
 const GaugeChart = ({ min, max, title, valueSuffix, measure }) => {
     const [chartOptions, setChartOptions] = useState(null);
 
-    const gaugeOptions = {
-        chart: {
-            type: 'solidgauge',
-        },
-        title: {
-            text: title,
-        },
-        pane: {
-            center: ['50%', '30%'],
-            size: '100%',
-            startAngle: -90,
-            endAngle: 90,
-            background: {
-                innerRadius: '60%',
-                outerRadius: '100%',
-                shape: 'arc',
-            },
-        },
-        exporting: {
-            enabled: false,
-        },
-        tooltip: {
-            enabled: false,
-        },
-        yAxis: {
-            min: 0,
-            max: 10,
-            stops: [
-                [0, '#01fb0a'],
-                [0.5, '#FFA500'],
-                [1, '#ff0032'],
-            ],
-            lineWidth: 0,
-            tickWidth: 0,
-            minorTickInterval: null,
-            tickAmount: 2,
-            title: {
-                y: -70,
-            },
-            labels: {
-                y: 16,
-            },
-        },
-        plotOptions: {
-            solidgauge: {
-                dataLabels: {
-                    y: 5,
-                    borderWidth: 0,
-                    useHTML: true,
-                },
-            },
-        },
-    };
-
     useEffect(() => {
-        const chartOptions = {
+        const gaugeOptions = {
             chart: {
+                type: 'solidgauge',
                 height: '400px',
                 width: 200,
-                backgroundColor: 'transparent'
+                backgroundColor: 'transparent',
             },
-            credits: {
+            title: {
+                text: title,
+            },
+            pane: {
+                center: ['50%', '30%'],
+                size: '100%',
+                startAngle: -90,
+                endAngle: 90,
+                background: {
+                    innerRadius: '60%',
+                    outerRadius: '100%',
+                    shape: 'arc',
+                },
+            },
+            exporting: {
                 enabled: false,
             },
-            series: [{
-                data: [measure],
-                dataLabels: {
-                    format:
-                        '<div style="text-align:center"></br>' +
-                        '<span style="font-size:19px"><b>{y}</b></span><br/>' +
-                        `<span style="font-size:18px"><b>${valueSuffix}</b></span>` +
-                        '</div>',
-                },
-                tooltip: {
-                    valueSuffix: valueSuffix,
-                },
-            }],
+            tooltip: {
+                enabled: false,
+            },
             yAxis: {
                 min: min,
                 max: max,
                 tickInterval: 0,
+                stops: [
+                    [0, '#01fb0a'],
+                    [0.5, '#FFA500'],
+                    [1, '#ff0032'],
+                ],
+                lineWidth: 0,
+                tickWidth: 0,
+                minorTickInterval: null,
+                tickAmount: 2,
+                title: {
+                    y: -70,
+                },
+                labels: {
+                    y: 16,
+                },
             },
+            plotOptions: {
+                solidgauge: {
+                    dataLabels: {
+                        y: 5,
+                        borderWidth: 0,
+                        useHTML: true,
+                    },
+                },
+            },
+            series: [
+                {
+                    data: [measure],
+                    dataLabels: {
+                        format:
+                            '<div style="text-align:center"></br>' +
+                            '<span style="font-size:19px"><b>{y}</b></span><br/>' +
+                            `<span style="font-size:18px"><b>${valueSuffix}</b></span>` +
+                            '</div>',
+                    },
+                    tooltip: {
+                        valueSuffix: valueSuffix,
+                    },
+                },
+            ],
         };
 
-        const mergedOptions = Highcharts.merge(gaugeOptions, chartOptions);
-
-        setChartOptions(mergedOptions);
+        setChartOptions(gaugeOptions);
     }, [min, max, title, valueSuffix, measure]);
 
     return (
